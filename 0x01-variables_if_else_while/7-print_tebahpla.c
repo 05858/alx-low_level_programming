c69b7d74c39bc1f8c521e1e65f98487f9cab3742
05858 <mohnasr5123@gmail.com>
2023-09-08 08:41:09 -0700

    print alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,10 +3,21 @@
 /**
  * main - Entry point
  *
- * Description:
+ * Description: print a-z in reverse
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
