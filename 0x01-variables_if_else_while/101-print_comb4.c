5066720ff578b188d646fb180af2e5706ee5d7f6
05858 <mohnasr5123@gmail.com>
2023-09-08 12:16:40 -0700

    comb advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 			digit3 = 0;
 			while (digit3 <= 9)
 			{
-				if (digit1 != digi2 &&
+				if (digit1 != digit2 &&
 					digit1 < digit2 &&
 					digit2 != digit3 &&
 					digit2 < digit3)
