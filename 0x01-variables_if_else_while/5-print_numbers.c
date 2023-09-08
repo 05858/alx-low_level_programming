9d56a807923e8245f8f55638fe1570f7eac7a7b2
05858 <mohnasr5123@gmail.com>
2023-09-08 08:26:02 -0700

    print alphabets

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,10 +3,21 @@
 /**
  * main - Entry point
  *
- * Description:
+ * Description: print all single digit numbers
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
