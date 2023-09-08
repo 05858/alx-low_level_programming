d1f3bbce05a19793f7e0c084d35656859eeb7a98
05858 <mohnasr5123@gmail.com>
2023-09-08 08:35:20 -0700

    print alphabets

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,10 +3,23 @@
 /**
  * main - Entry point
  *
- * Description:
+ * Description: print 0-9 using putchar
+ *		while using int variable
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
