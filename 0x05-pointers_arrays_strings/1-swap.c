4e12933aa4a20d9d6eb98cfaa5f4cc5572972615
05858 <mohnasr5123@gmail.com>
2023-09-18 03:05:23 -0700

    C array pointers 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * swap_int - swaps the valiues of two integars
+ *		using two input parameters
+ *
+ * @a: input parameters 1
+ * @b: input parameters 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
