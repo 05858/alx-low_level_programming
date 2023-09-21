05bf4c48f1200c2fcf674fc6565ad8a41ff218c2
05858 <mohnasr5123@gmail.com>
2023-09-21 01:53:24 -0700

    C more pointers arrays 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,25 +3,31 @@
 /**
  * _strcat - function that concatenates two strings.
  *
- * @dest: pointer to destnation input
- * @src: pointer to source input
+ * @dest: input vlaue
+ * @src: input value
  *
- * Return: pointer to resulting string @dest
+ * Return: void
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
+	int i;
+	int j;
 
-	C = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-		c++;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-	/* iterate through each src array value without the null byte */
-	for (c2 = 0; src[c2]; c2++)
-		/*append src[c2] to dest|c| while overwritting the null byte in dest*/
-		dest[C++] = src[c2];
+	dest[i] = '\0';
 
 	return (dest);
 }
